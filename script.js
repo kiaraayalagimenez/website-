e2998e0bf2ce539f8f42899e26d7557f21e3523d
kiaraayalagimenez <138132382+kiaraayalagimenez@users.noreply.github.com>
2023-06-29 08:48:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var haspets = true;
+var numberofsiblings = 1;
+
+
+console. log(numberofsiblings) ;
+console. log(haspets) ;
+
+
+
+if (numberofsiblings === 1){
+    console.log(" byeee  ");
+
+}
+
+var number = 0;
+
+while (number < 15){
+console.log("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15");
+number++;
+
+}
+
+var button = document.getElementById("hi")
+var body = document.body;
+
+body.setAttribute("style" , "background-color: purple")
+
+button.addEventListener("click", function(){
+    console.log("click");
+    body.setAttribute("style",
+    "background-color: pink")
+
+})
+
+
+
+
+
+
+
